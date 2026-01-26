fa54a13276f482f0280f0c8188946126fd400fcb
Ilya Dashkov <daschkov.id15@physics.msu.ru>
2026-01-26 18:28:48 +0300

    Moved to lifespan from @app.on_event("startup")

---
+++ development/src/main.py
@@ -0,0 +1,38 @@
+import joblib
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, File, UploadFile
+from fastapi.testclient import TestClient
+from src.db import insert_credit_record, init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    app.state.model = joblib.load(
+        "/app/model/credit_default_pipeline.joblib"
+    )
+    yield
+
+app = FastAPI(debug=True, lifespan=lifespan)
+
+from src.schemas import CreditRecordIn, PredictionOut
+import pandas as pd
+
+@app.get("/test")
+def test(name: str = "Guest"):
+    return {"message": f"Hello, {name} ! The server test is success!"}
+
+@app.post("/predict")
+def predict(payload: dict):
+    X = pd.DataFrame([payload])
+    proba = app.state.model.predict_proba(X)[0, 1]
+    return {"default_probability": float(proba)}
+
+@app.post("/uploadfile/")
+def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+@app.post("/load_data_from_csv")
+def load_data_from_csv(payload: dict):
+    X = pd.DataFrame([payload])
+    proba = app.state.model.predict_proba(X)[0, 1]
+    return {"default_probability": float(proba)}
