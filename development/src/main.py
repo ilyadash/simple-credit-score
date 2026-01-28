d3eec002cc98ffe35e72ffe9df88f39d38de7c33
Ilya Dashkov <daschkov.id15@physics.msu.ru>
2026-01-28 20:02:49 +0300

    added my preprocessor class

--- development/src/main.py
+++ development/src/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.testclient import TestClient
 from src.db import insert_credit_record, init_db
+import my_processor
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
