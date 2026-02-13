e25bbeef4056a0f70683e52d60aeacf7ebc5e318
Ilya Dashkov <69359690+ilyadash@users.noreply.github.com>
2026-02-13 22:33:03 +0300

    Added saving of processed data in the database

--- development/backend/src/main.py
+++ development/backend/src/main.py
@@ -6,7 +6,7 @@ from io import StringIO, BytesIO
 from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.testclient import TestClient
 from fastapi.responses import StreamingResponse
-from src.db import insert_credit_record, init_db
+from src.db import insert_credit_record, init_db, bulk_insert_credit_records
 from src.my_processor import MyDataPreprocessor
 
 # Add the app directory to Python path to resolve module imports
@@ -49,7 +49,8 @@ async def predict(data: CreditRecord):
 async def predict_file(file: UploadFile = File(...)):
     # 1. Читаем входящий файл в Pandas
     contents = await file.read()
-    X = pd.read_csv(BytesIO(contents)) # Используем BytesIO, так как UploadFile.file — это бинарный поток
+    provided_data = pd.read_csv(BytesIO(contents)) # Используем BytesIO, так как UploadFile.file — это бинарный поток
+    X = provided_data.copy(deep=True)
     # 2. Подготавливаем данные перед подачей в модель
     if 'loan_status' in X.columns:
         X = X.drop(columns='loan_status')
@@ -59,11 +60,16 @@ async def predict_file(file: UploadFile = File(...)):
     probs_and_predictions = pd.DataFrame()
     probs_and_predictions['default_probability'] = pd.Series(probability)
     probs_and_predictions['pred_class'] = pd.Series(answer)
-    # 4. Подготовка ответа без сохранения на диск
+    # 4. Сохраянем данные по кредиту и предсказанию для него в базу
+    processed_data = provided_data.copy(deep=True)
+    processed_data['default_probability'] = pd.Series(probability)
+    processed_data['pred_class'] = pd.Series(answer)
+    bulk_insert_credit_records(processed_data)
+    # 5. Подготовка ответа без сохранения на диск
     stream = StringIO()
     probs_and_predictions.to_csv(stream, index=False)
     
-    # Создаем ответ и добавляем заголовки для скачивания
+    # 6. Создаем ответ и добавляем заголовки для скачивания
     response = StreamingResponse(
         iter([stream.getvalue()]),
         media_type="text/csv"
